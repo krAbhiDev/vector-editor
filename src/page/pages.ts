3164ea3b220f1679ab425365ac9bebb8c3688176
kr-abhi55 <abhishek33gl@gmail.com>
2023-11-10 21:45:33 +0530

    added MathEval with lexer . ast is generated with recursive descent parsing

--- src/page/pages.ts
+++ src/page/pages.ts
@@ -2,6 +2,7 @@ import React from "react"
 import PanZoom from "./PanZoom"
 import PaperDemo from "./PaperDemo"
 import ConvexHull from "./ConvexHull"
+import MathEval from "./MathEval"
 
 type PageItem = {
     name: string
@@ -11,6 +12,7 @@ const pages: PageItem[] = [
     { name: "pan-zoom", component: PanZoom },
     { name: "paper-demo", component: PaperDemo },
     { name: "convex-hull", component: ConvexHull },
+    { name: "math-eval", component: MathEval },
 ]
 
 export default pages
